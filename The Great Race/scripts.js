ec85bbe6fa471dbe5971261728f711eb792bded9
Lucas <lucasrclark@gmail.com>
2021-11-26 01:54:24 -0600

    added other files

---
+++ The Great Race/scripts.js
@@ -0,0 +1,40 @@
+
+//initializing racer objects 
+const racer1 = {
+  initialPosition: 0,
+  position: 0,
+  minSpeed: 2,
+  maxSpeed: 4,
+}
+
+const racer2 = {
+  initialPosition: 0,
+  position: 0,
+  minSpeed: 2,
+  maxSpeed: 4,
+}
+
+const racer3 = {
+  initialPosition: 0,
+  position: 0,
+  minSpeed: 2,
+  maxSpeed: 4,
+}
+
+const racer4 = {
+  initialPosition: 0,
+  position: 0,
+  minSpeed: 2,
+  maxSpeed: 4,
+}
+
+//getting initial positions of racers
+var x = document.getElementById('racer1');
+racer1['initialPosition'] = x.offsetLeft
+racer1['position'] = x.offsetLeft
+racer2['initialPosition'] = x.offsetLeft
+racer2['position'] = x.offsetLeft
+racer3['initialPosition'] = x.offsetLeft
+racer3['position'] = x.offsetLeft
+racer4['initialPosition'] = x.offsetLeft
+racer4['position'] = x.offsetLeft
