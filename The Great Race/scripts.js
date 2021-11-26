d81d1d78914917ca3de4ab4b415dcfd02b7368f9
Lucas <lucasrclark@gmail.com>
2021-11-26 04:14:06 -0600

    finished project(?)

--- The Great Race/scripts.js
+++ The Great Race/scripts.js
@@ -1,40 +1,123 @@
-
+let winner = 0
 //initializing racer objects 
 const racer1 = {
-  initialPosition: 0,
+  name: 'racer1',
   position: 0,
-  minSpeed: 2,
-  maxSpeed: 4,
+  minSpeed: 1,
+  maxSpeed: 10,
 }
 
 const racer2 = {
-  initialPosition: 0,
+  name: 'racer2',
   position: 0,
-  minSpeed: 2,
-  maxSpeed: 4,
+  minSpeed: 1,
+  maxSpeed: 10,
 }
 
 const racer3 = {
-  initialPosition: 0,
+  name: 'racer3',
   position: 0,
-  minSpeed: 2,
-  maxSpeed: 4,
+  minSpeed: 1,
+  maxSpeed: 10,
 }
 
 const racer4 = {
-  initialPosition: 0,
+  name: 'racer4',
   position: 0,
-  minSpeed: 2,
-  maxSpeed: 4,
+  minSpeed: 1,
+  maxSpeed: 10,
+}
+//getting variables for html items
+img1 = document.getElementById('racer1')
+img2 = document.getElementById('racer2')
+img3 = document.getElementById('racer3')
+img4 = document.getElementById('racer4')
+
+winnerDiv = document.getElementById('winnerDiv')
+winnerImg = document.getElementById('winnerImg')
+winnerText = document.getElementById('winnerText')
+stoplight = document.getElementById('stoplight')
+
+//adding event for clicking the stoplight
+stoplight.addEventListener('click', function() {
+
+  stoplight.src = "./images/green.png"
+
+  //setting racers to walking animation
+  img1.src = "./images/walk1.gif"
+  img2.src = "./images/walk2.gif"
+  img3.src = "./images/walk3.gif"
+  img4.src = "./images/walk4.gif"
+
+  //adding interval to move racers
+  let raceInterval = setInterval(function(){
+    //calling functions to move racers
+    moveImg(racer1)
+    moveImg(racer2)
+    moveImg(racer3)
+    moveImg(racer4)
+    
+    //checking for winner
+    if (racer1['position'] >= 800){
+      winner = 1
+      winnerImg.src = "./images/1.gif"
+    }
+    else if (racer2['position'] >= 800){
+      winner = 2
+      winnerImg.src = "./images/2.gif"
+    }
+    else if (racer3['position'] >= 800){
+      winner = 3
+      winnerImg.src = "./images/3.gif"
+    }
+    else if (racer4['position'] >= 800){
+      winner = 4
+      winnerImg.src = "./images/4.gif"
+    }
+    
+    if (winner != 0){
+      winnerText.innerHTML += winner
+      clearInterval(raceInterval)
+      //resetting images to idle animation
+      img1.src = "./images/1.gif"
+      img2.src = "./images/2.gif"
+      img3.src = "./images/3.gif"
+      img4.src = "./images/4.gif"
+      //displaying winner
+      winnerDiv.style.display = 'initial'
+
+      winnerDiv.addEventListener('click', function(){
+        //resetting stoplight and winner display
+        stoplight.src = "./images/red.png"
+        winnerDiv.style.display = 'none'
+        winnerText.innerHTML = 'Winner is #'
+        //reseting positions of all racers and winner variable
+        resetImg(racer1)
+        resetImg(racer2)
+        resetImg(racer3)
+        resetImg(racer4)
+        winner = 0
+      })
+    }
+  //setting interval for 16ms
+  }, 16)
+
+})
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-//getting initial positions of racers
-var x = document.getElementById('racer1');
-racer1['initialPosition'] = x.offsetLeft
-racer1['position'] = x.offsetLeft
-racer2['initialPosition'] = x.offsetLeft
-racer2['position'] = x.offsetLeft
-racer3['initialPosition'] = x.offsetLeft
-racer3['position'] = x.offsetLeft
-racer4['initialPosition'] = x.offsetLeft
-racer4['position'] = x.offsetLeft
+function moveImg(object) {
+  //getting random integer to move racer
+  var x = getRndInteger(object['minSpeed'], object['maxSpeed'])
+  //moving racer + adding to position variable
+  document.getElementById(object['name']).style.left = object['position'] + x + "px"
+  object['position'] = object['position'] + x
+}
+
+function resetImg(object) {
+  //resetting position on screen and position variable to 0
+  document.getElementById(object['name']).style.left = 0 + "px"
+  object['position'] = 0
+}
